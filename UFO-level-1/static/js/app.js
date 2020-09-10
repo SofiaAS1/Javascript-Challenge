cc960963395247e1b402329bc1566a317c003d2f
SAS 826 <sasotillo826@gmail.com>
2020-09-09 20:36:41 -0400

    My Javascript so far

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,30 @@
 // from data.js
 var tableData = data;
+var tbody = d3.select('tbody');
+var btn = d3.select('button');
 
-// YOUR CODE HERE!
+renderTable(tableData);
+
+btn.on("click", handleClick);
+
+function handleClick() {
+    // console.log('Hello');
+    d3.event.preventDefault();
+    var filteredData = tableData;
+    var input = d3.select('#datetime');
+    var inputval = input.property("value");
+    filteredData = tableData.filter(row => row.datetime === inputval);
+    console.log(filteredData);
+    renderTable(filteredData);
+}
+
+function renderTable(data) {
+    data.forEach(obj => {
+        var row = tbody.append('tr');
+        Object.values(obj).forEach(val => {
+            row.append('td').text(val);
+        })
+    });
+};
+
+
