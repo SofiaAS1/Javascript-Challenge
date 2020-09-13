f06682686761cf42fc2569b7a99bfd86de799546
SAS 826 <sasotillo826@gmail.com>
2020-09-13 16:46:59 -0400

    Updaated code & Finished Level 1 of HW

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -3,10 +3,13 @@
 var tableData = data;
 var btn = d3.select('button');
 var tbody = d3.select('tbody');
+var form = d3.select('form');
+
 
 renderTable(tableData);
 
 btn.on("click", handleClick);
+form.on("submit", handleClick);
 
 function handleClick() {
     d3.event.preventDefault();
@@ -15,6 +18,7 @@ function handleClick() {
     var inputval = input.property("value");
     filteredData = tableData.filter(row => row.datetime === inputval);
     console.log(filteredData);
+    tbody.html("");
     renderTable(filteredData);
 }
 
@@ -27,4 +31,4 @@ function renderTable(data) {
     });
 };
 
-
+ 
