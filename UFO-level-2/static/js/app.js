a03a84d7ad5622b20c6b15de630565e204518929
SAS 826 <sasotillo826@gmail.com>
2020-09-13 22:29:49 -0400

    Got Level 2 to use multiple filters

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -3,44 +3,39 @@
 var tableData = data;
 var btn = d3.select('button');
 var tbody = d3.select('tbody');
-var form = d3.select('form');
-
 
 renderTable(tableData);
 
 btn.on("click", handleClick);
-form.on("submit", handleClick);
 
 function handleClick() {
     d3.event.preventDefault();
     var filteredData = tableData;
-    var input1 = d3.select('#Search-Categories');
+    var input1 = d3.select('#asdf').property("value");
     var input2 = d3.select('#info2')
     if (input1 === "Date") {
         var inputval = input2.property("value");
-        filteredData = tableData.filter(row => row.datetime === inputval)
-        tbody.html("")}
+        filteredData = tableData.filter(row => row.datetime === inputval);
+        tbody.html("");}
     else if (input1 === "City") {
-        var inputval = input2.property("value")
-        filteredData = tableData.filter(row => row.city === inputval)
-        tbody.html("")}
+        var inputval = input2.property("value");
+        filteredData = tableData.filter(row => row.city === inputval);
+        tbody.html("");}
     else if (input1 === "State") {
         var inputval = input2.property("value");
-        filteredData = tableData.filter(row => row.state === inputval)
-        tbody.html("")}   
+        filteredData = tableData.filter(row => row.state === inputval);
+        tbody.html("");}   
     else if (input1 === "Country") {
         var inputval = input2.property("value");
-        filteredData = tableData.filter(row => row.country === inputval)
-        tbody.html("")}
+        filteredData = tableData.filter(row => row.country === inputval);
+        tbody.html("");}
     else if (input1 === "Shape") {
         var inputval = input2.property("value");
-        filteredData = tableData.filter(row => row.shape === inputval)
-        tbody.html("")}
+        filteredData = tableData.filter(row => row.shape === inputval);
+        tbody.html("");}
     else {console.log ("YOU LOSE, CHARLIE, YOU LOSE!!!")}
-    tbody.html("");
     console.log(filteredData);
     renderTable(filteredData);
-
 }
 
 function renderTable(data) {
